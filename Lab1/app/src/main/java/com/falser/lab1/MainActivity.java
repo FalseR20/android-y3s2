7cd5ceaffa57af7a070cf060b159fe530ec49db2
FalseR20 <kno3.2002@gmail.com>
2023-02-18 12:52:06 +0300

    feat(lab1): add randomizing pairs

--- Lab1/app/src/main/java/com/falser/lab1/MainActivity.java
+++ Lab1/app/src/main/java/com/falser/lab1/MainActivity.java
@@ -3,6 +3,7 @@ package com.falser.lab1;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -10,8 +11,16 @@ import android.widget.TableRow;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     final int ROWS_COUNT = 6;
+    final int IMAGES_COUNT = ROWS_COUNT * ROWS_COUNT;
+    final int PAIRS_COUNT = IMAGES_COUNT / 2;
+
+    List<Integer> PAIRS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +35,16 @@ public class MainActivity extends AppCompatActivity {
             }
             imagesRows.addView(imageRow, i);
         }
+        restart();
+    }
+
+    private void restart() {
+        Integer[] intsArray = new Integer[IMAGES_COUNT];
+        for (int i = 0; i < PAIRS_COUNT; i++)
+            intsArray[2 * i] = intsArray[2 * i + 1] = i;
+        PAIRS = Arrays.asList(intsArray);
+        Collections.shuffle(PAIRS);
+        Log.d("restart()", String.format("Random ints: %s", PAIRS));
     }
 }
 
