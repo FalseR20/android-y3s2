007d347e2868a89647cde873f8ae103c36c718f5
FalseR20 <kno3.2002@gmail.com>
2023-02-18 16:16:49 +0300

    feat(lab1): add pairs logic

--- Lab1/app/src/main/java/com/falser/lab1/MainActivity.java
+++ Lab1/app/src/main/java/com/falser/lab1/MainActivity.java
@@ -4,21 +4,24 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Button;
+import android.view.View;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    final int ROWS_COUNT = 6;
+    final int ROWS_COUNT = 4;
     final int IMAGES_COUNT = ROWS_COUNT * ROWS_COUNT;
     final int PAIRS_COUNT = IMAGES_COUNT / 2;
+
+    TableLayout imagesRows;
 
     List<Integer> PAIRS;
 
@@ -26,11 +29,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TableLayout imagesRows = findViewById(R.id.images);
+        imagesRows = findViewById(R.id.images);
         for (int i = 0; i < ROWS_COUNT; i++) {
             TableRow imageRow = new TableRow(this);
             for (int j = 0; j < ROWS_COUNT; j++) {
-                Button imageButton = new ImageButton(this);
+                ImageButton imageButton = new ImageButton(this);
                 imageRow.addView(imageButton, j);
             }
             imagesRows.addView(imageRow, i);
@@ -38,7 +41,19 @@ public class MainActivity extends AppCompatActivity {
         restart();
     }
 
-    private void restart() {
+    public void restart() {
+        updateRandom();
+        TableRow row;
+        for (int i = 0; i < ROWS_COUNT; i++) {
+            row = (TableRow) imagesRows.getChildAt(i);
+            for (int j = 0; j < ROWS_COUNT; j++) {
+                ImageButton image = (ImageButton) row.getChildAt(j);
+                image.number = PAIRS.get(i * ROWS_COUNT + j);
+            }
+        }
+    }
+
+    private void updateRandom() {
         Integer[] intsArray = new Integer[IMAGES_COUNT];
         for (int i = 0; i < PAIRS_COUNT; i++)
             intsArray[2 * i] = intsArray[2 * i + 1] = i;
@@ -46,9 +61,20 @@ public class MainActivity extends AppCompatActivity {
         Collections.shuffle(PAIRS);
         Log.d("restart()", String.format("Random ints: %s", PAIRS));
     }
+
+    public void restartClick(View view) {
+        restart();
+    }
 }
 
-class ImageButton extends AppCompatButton {
+class ImageButton extends AppCompatButton implements View.OnClickListener {
+    public int number;
+    public static final int STATE_COUNT = 2;
+    public static final int STATE_DEFAULT = STATE_COUNT - 1;
+
+    public static List<ImageButton> numbersButtons = new ArrayList<>();
+
+    public static int state = STATE_DEFAULT;
 
     public ImageButton(Context context) {
         super(context);
@@ -56,11 +82,40 @@ class ImageButton extends AppCompatButton {
         params.weight = 1;
         params.rightMargin = params.leftMargin = 5;
         setLayoutParams(params);
+        setOnClickListener(this);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         setHeight(getWidth());
         super.onDraw(canvas);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (numbersButtons.contains(this)) {
+            return;
+        }
+        if (state == -1) {
+            state = STATE_DEFAULT;
+
+            if (!checkAllEqual()) {
+                for (ImageButton button: numbersButtons)
+                    button.setText("");
+            }
+            numbersButtons.clear();
+        }
+        numbersButtons.add(this);
+        state--;
+        setText(String.valueOf(number));
+    }
+
+    boolean checkAllEqual() {
+        int check = numbersButtons.get(0).number;
+        for (ImageButton button: numbersButtons) {
+            if (check != button.number)
+                return false;
+        }
+        return true;
     }
 }
